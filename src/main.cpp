40a73673f3a95e0c0ec2f40060c683ff416104b1
fishPointer <iantralmer@gmail.com>
2022-05-11 14:36:47 -0700

    Finally got type conversion working properly

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@ const char *SSID = "Breath of the Wifi";
 const char *PWD = "supervolcano55";
 const char *HUTAO = "192.168.0.193";
 const char *TOPIC_CORN = "corn";
+const char *TOPIC_BOOLY = "booly";
 int mqttPort = 1616;
 
 WiFiClient wifiClient;
@@ -33,13 +34,34 @@ void loop() {
 ////////////////////////
 
 void callback(char* topic, byte* payload, unsigned int length) {
-int callbackmode = 0;
 
+//Initialize some Variables for switching and payload conversion
+int callbackmode = 0;
+int num_payload = -1;
+String str_payload;
+char buffer_payload[length];
+char out_msg[40];
+
+//Saving Payload into a buffer, and String
+for (int i = 0; i < length; i++) 
+{
+  buffer_payload[i] = char(payload[i]);
+}
+str_payload = String(buffer_payload);
+
+//Will replace with enum, and integrate into the switch
 if(strcmp(topic, TOPIC_CORN) == 0)
 {
   callbackmode = 2;
 }
 
+if(strcmp(topic, TOPIC_BOOLY) == 0)
+{
+  callbackmode = 3;
+}
+
+//Primary switch statement for different functions by topic
+//Master list
   switch(callbackmode)
   {
     case 1:
@@ -53,6 +75,17 @@ if(strcmp(topic, TOPIC_CORN) == 0)
       mqttClient.publish("feedback", TOPIC_CORN);
       break;
 
+    case 3:
+      Serial.println("booly trigger");
+      num_payload = str_payload.toInt();
+      num_payload = 5*num_payload;
+      itoa(num_payload, out_msg, 10);
+      Serial.print("calc output: ");
+      Serial.println(out_msg);
+      mqttClient.publish("feedback", out_msg);
+      break;
+
+    //Default Functionality, read off message and topic
     default:
       Serial.print("Callback - ");
       Serial.println(topic);
@@ -63,11 +96,8 @@ if(strcmp(topic, TOPIC_CORN) == 0)
         Serial.print(char(payload[i]));
       }
       Serial.println();
-      Serial.println(length);
   }
   
-
-
 }
 
 ////////////////////////
@@ -101,7 +131,7 @@ void reconnect() {
         // subscribe to topic
         mqttClient.subscribe("corn");
         mqttClient.subscribe("potato");
-        //mqttClient.subscribe("feedback");
+        mqttClient.subscribe("booly");
       }
       
   }
